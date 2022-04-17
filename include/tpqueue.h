a52aa50efeca7242d8945e917d38bdb24678b53b
GabovaMasha <94718616+GabovaMasha@users.noreply.github.com>
2022-04-17 23:19:51 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,15 +1,16 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
+#include <string>
 
 template<typename T, int size>
 class TPQueue {
-private:
+ private:
 T *arr;
 int first;
 int last;
- 
-public:
+
+ public:
 TPQueue() :
 first(0), last(0) {
 arr = new T[size];
